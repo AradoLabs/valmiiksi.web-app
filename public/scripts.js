462a0c1ebf7a65b90764d9115fe2dd55bf65f726
vilppu <tsvilp@gmail.com>
2017-11-07 17:22:41 +0200

    basic layout and logic

--- public/scripts.js
+++ public/scripts.js
@@ -1,53 +1,78 @@
-const httpSend = (method, url) => {
+(function () {
+    const httpSend = (method, url) => {
 
-    return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => {
 
-        const request = new XMLHttpRequest();
+            const request = new XMLHttpRequest();
 
-        const onLoaded = e => {
-            if (request.readyState === 4) {
-                resolve(request.responseText);
-            }
-        };
+            const onLoaded = e => {
+                if (request.readyState === 4) {
+                    resolve(request);
+                }
+            };
 
-        const onError = e => {
-            if (request.readyState === 4) {
-                reject(request.statusText);
-            }
-        };
+            const onError = e => {
+                if (request.readyState === 4) {
+                    reject(request);
+                }
+            };
 
-        request.addEventListener("load", onLoaded);
-        request.addEventListener("error", onError);
-        request.overrideMimeType("application/json");
-        request.open(method, url, true);
-        // request.setRequestHeader("Authorization", "bearer " + token);
-        request.send(null);
-    });
-};
-
-const httpGet = (url) => {
-    return httpSend("GET", url);
-};
-
-const tradeClicked = trade => {
-    const url = "http://localhost:10888/api/profiles";
-
-    httpGet(url).then(json => {
-        const profiles = JSON.parse(json);
-        const profilesElement = document.getElementById("profiles");
-
-        profilesElement.innerText = "";
-
-        profiles.forEach(function (profile) {
-            profilesElement.innerText += profile.name + ": " + profile.trade;
+            request.addEventListener("load", onLoaded);
+            request.addEventListener("error", onError);
+            request.overrideMimeType("application/json");
+            request.open(method, url, true);
+            // request.setRequestHeader("Authorization", "bearer " + token);
+            request.send(null);
         });
-    }).catch(error => {
-        console.log(error);
-    });
-};
+    };
 
-const trades = document.getElementById("trades");
+    const httpGet = (url) => {
+        return httpSend("GET", url);
+    };
 
-trades.childNodes.forEach(function (trade) {
-    trade.addEventListener("click", tradeClicked, false);
-});
+    const reload = (area, profession) => {
+        const url = "http://localhost:10888/api/companies/" + area + "/" + profession;
+        const template = document.getElementById("company-template");
+
+        httpGet(url)
+            .then(request => {
+                const companies = JSON.parse(request.responseText);
+                const profilesElement = document.getElementById("profiles");
+
+                profilesElement.innerHTML = "";
+
+                companies.forEach(function (company) {
+                    const clonedTemplate = document.importNode(template.content, true);
+                    const companyElement = clonedTemplate.querySelector(".company");
+                    const companyNameElement = companyElement.querySelector(".name");
+
+                    companyNameElement.innerText = company.name;
+
+                    profilesElement.appendChild(companyElement);
+                });
+            });
+    };
+
+    const professionSelector = () => {
+        const professionElements = Array.from(document.getElementsByClassName("profession"));
+
+        var professionClicked = (selectedProfessionElement) => () => {
+
+            professionElements.forEach((professionElement) => {
+                professionElement.classList.remove("selected");
+            });
+            selectedProfessionElement.classList.add("selected");
+            
+            const selectedArea = document.getElementById("area").innerText;
+            const selectedProfession = selectedProfessionElement.getAttribute("data-profession");
+
+            reload(selectedArea, selectedProfession);
+        };
+
+        professionElements.forEach(function (professionElement) {
+            professionElement.addEventListener("click", professionClicked(professionElement));
+        });
+    };
+
+    professionSelector();
+})();
